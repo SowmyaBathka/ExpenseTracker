b1ab4882a8edf7f242f7e19b52ad6dcd0e0211fb
Bathka Sowmya <bathkasowmya@gmail.com>
2026-05-20 19:31:32 +0530

    Added expense entry functionality

--- expense_operations.py
+++ expense_operations.py
@@ -1,5 +1,23 @@
+from data_store import expenses
+
+
 def add_expense():
-    print("\nAdd Expense feature will be implemented tomorrow.")
+    print("\n--- Add Expense ---")
+
+    amount = input("Enter Amount: ")
+    category = input("Enter Category: ")
+    description = input("Enter Description: ")
+
+    expense = {
+        "amount": amount,
+        "category": category,
+        "description": description
+    }
+
+    expenses.append(expense)
+
+    print("Expense added successfully!")
+
 
 def view_expenses():
     print("\nView Expenses feature will be implemented later.")
